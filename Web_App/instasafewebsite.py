eb50b7baf8a86269c2f376f44b8f8afc1470c354
Kyle Mumma <kyle.mumma@live.com>
2018-11-24 15:21:56 -0800

    Web App Running
    I completed the web app it is up and running. There is no display for the results yet though, that is exclusively in console. Also it seems the output array for the language processing is not right

--- Web_App/instasafewebsite.py
+++ Web_App/instasafewebsite.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from forms import LoginForm
+from forms import LoginForm, InstasafeForm
+import sys
+
+sys.path.append('../webscraper/')
+from Instasafe import Instasafe
 
 app = Flask(__name__)
 
@@ -17,9 +21,23 @@ def home():
             flash("Incorrect Login Info")
     return render_template("home.htm", form=form)
 
-@app.route("/instasafe-logged-in-successfully?=0508436b60727130275c01c80f493267")
+@app.route("/instasafe-logged-in-successfully?=0508436b60727130275c01c80f493267", methods=["GET", "POST"])
 def instasafe():
-    return render_template("instasafe.htm")
+    form = InstasafeForm()
+
+    if(form.validate_on_submit()):
+        usernames_string = form.username_list.data.replace(" ", "")
+        usernames_array = usernames_string.split(",")
+
+        output_file = open("../webscraper/usernames.txt", "w")
+        for username in usernames_array:
+            output_file.write(username + "\n")
+        output_file.close()
+
+        instasafe = Instasafe()
+        instasafe.run()
+
+    return render_template("instasafe.htm", form=form)
 
 if(__name__ == "__main__"):
     app.run(debug=True)
--- Webscraper/Instasafe.py
+++ Webscraper/Instasafe.py
@@ -5,49 +5,51 @@ from InstagramScraper import InstagramScraper
 from ImageClassifier import ImageClassifier
 from LanguageProcessor import LanguageProcessor
 
-#Image Classification
-def imageClassification(account_data, output):
-    imageClassifier = ImageClassifier()
-    image_classifier_output = imageClassifier.classify(account_data)
-    output.append(image_classifier_output)
-    print(image_classifier_output)
+class Instasafe:
+    #Image Classification
+    def imageClassification(self, account_data, output):
+        imageClassifier = ImageClassifier()
+        image_classifier_output = imageClassifier.classify(account_data)
+        output.append(image_classifier_output)
+        print(image_classifier_output)
 
-#Natural Language Processing
-def naturalLanguageProcessing(account_data, output):
-    languageProcessor = LanguageProcessor()
-    language_processor_output = languageProcessor.process(account_data)
-    output.append(language_processor_output)
-    print(language_processor_output)
+    #Natural Language Processing
+    def naturalLanguageProcessing(self, account_data, output):
+        languageProcessor = LanguageProcessor()
+        language_processor_output = languageProcessor.process(account_data)
+        output.append(language_processor_output)
+        print(language_processor_output)
 
-#set all file paths relative to the location of this program
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+    def run(self):
+        #set all file paths relative to the location of this program
+        abspath = os.path.abspath(__file__)
+        dname = os.path.dirname(abspath)
+        os.chdir(dname)
 
-#Instagram Scraping
-instaScraper = InstagramScraper()
-account_data = instaScraper.scrape("usernames.txt")
+        #Instagram Scraping
+        instaScraper = InstagramScraper()
+        account_data = instaScraper.scrape("usernames.txt")
 
-print("Instagram Scraping Complete")
+        print("Instagram Scraping Complete")
 
-#initialize output variables for image classification and natural language processing
-image_classifier_output_raw = []
-language_processor_output_raw = []
+        #initialize output variables for image classification and natural language processing
+        image_classifier_output_raw = []
+        language_processor_output_raw = []
 
-#create a thread for image classigication
-thread_imageClassification = Thread(target = imageClassification, args = (account_data, image_classifier_output_raw,))
-thread_imageClassification.start()
+        #create a thread for image classigication
+        thread_imageClassification = Thread(target = self.imageClassification, args = (account_data, image_classifier_output_raw,))
+        thread_imageClassification.start()
 
-#create a thread for natural language processing
-thread_naturalLanguageProcessing = Thread(target = naturalLanguageProcessing, args = (account_data, language_processor_output_raw,))
-thread_naturalLanguageProcessing.start()
+        #create a thread for natural language processing
+        thread_naturalLanguageProcessing = Thread(target = self.naturalLanguageProcessing, args = (account_data, language_processor_output_raw,))
+        thread_naturalLanguageProcessing.start()
 
-#wait for all image classification and natural language processing to finish running
-thread_imageClassification.join()
-thread_naturalLanguageProcessing.join()
+        #wait for all image classification and natural language processing to finish running
+        thread_imageClassification.join()
+        thread_naturalLanguageProcessing.join()
 
-image_classifier_output = image_classifier_output_raw[0]
-language_processor_output = language_processor_output_raw[0]
+        image_classifier_output = image_classifier_output_raw[0]
+        language_processor_output = language_processor_output_raw[0]
 
-print(image_classifier_output)
-print(language_processor_output)
+        print(image_classifier_output)
+        print(language_processor_output)
